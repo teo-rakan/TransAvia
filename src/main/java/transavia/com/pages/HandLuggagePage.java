02301bfd487ffb4731e5ed527d1ca33f5874ce32
Alena Zubrevich <Alena_Zubrevich@epam.com>
2017-09-20 16:48:51 +0300

    Pause method was dropped

--- src/main/java/transavia/com/pages/HandLuggagePage.java
+++ src/main/java/transavia/com/pages/HandLuggagePage.java
@@ -4,9 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import transavia.com.core.BasePage;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class HandLuggagePage extends BasePage {
 
     @FindBy (xpath = "//*[contains(@src, 'youtube')]")
@@ -15,12 +12,6 @@ public class HandLuggagePage extends BasePage {
     public YoutubePage goToYoutubeSite() {
         String embeddedLink = videoFrame.getAttribute("src");
 
-        return new YoutubePage(getShortURLFromEmbedded(embeddedLink));
-    }
-
-    private String getShortURLFromEmbedded(String URL) {
-        Matcher matcher = Pattern.compile("(?<=embed/)[A-Za-z]+").matcher(URL);
-
-        return matcher.find() ? "https://youtu.be/" + matcher.group() : null;
+        return new YoutubePage("https://youtu.be/" + extractFrom(embeddedLink, "(?<=embed/)[A-Za-z]+"));
     }
 }
